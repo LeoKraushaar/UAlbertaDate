734fd2ff9ee4e728ec3438616f965e59418fcc2d
LEO KRAUSHAAR <LEO.KRAUSHAAR@gmail.com>
2022-11-05 13:51:35 -0600

    initialize all the apps, they don't do anything ye

--- UAlbertaDate/index/views.py
+++ UAlbertaDate/index/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
+BASE_URL = "http://127.0.0.1:8000/"
+
+def index(request):
+   context = {"account_url":BASE_URL + "account/"}
+   return None
