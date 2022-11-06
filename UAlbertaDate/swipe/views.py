1883ecd1147c54d4473768c096976adfa1be3daf
LEO KRAUSHAAR <LEO.KRAUSHAAR@gmail.com>
2022-11-06 03:39:46 -0700

    commit

--- UAlbertaDate/swipe/views.py
+++ UAlbertaDate/swipe/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from account.models import UserInfo
-from .forms import LikeForm, PassForm
+from .forms import LikeForm
 from .models import LikeRelationship
+from django.contrib.auth.models import User
 # Create your views here.
 
 STATIC_URL = "http://127.0.0.1:8000/static/media/{}"
@@ -16,7 +17,7 @@ def swipe(request):
         available_infos = UserInfo.objects.all()
         try:
             profile = available_infos.order_by("?").first()
-            if are_compatible(profile, user_info):
+            if are_compatible(profile, user_info) and is_viable(profile, user_info):
                 current_profile = profile
         except:
             context["user_image"] = None
@@ -31,6 +32,8 @@ def swipe(request):
         else:
             return redirect(SWIPE_URL)
 
+    
+    context["first_name"] = current_profile.user.first_name
     context["profile_info"] = current_profile.get_attributes()
     context["image_url"] = STATIC_URL.format(current_profile.image_file)
     return render(request, "swipe.html", context)
@@ -41,5 +44,13 @@ def are_compatible(info1, info2):
         assert info2.gender in info1.target_gender
         return True
     
+    except AssertionError:
+        return False
+
+def is_viable(profile, user_info):
+    try:
+        assert profile.user != user_info.user
+        assert not LikeRelationship.objects.get(liker=user_info.user, likee=profile.user)
+        return True
     except AssertionError:
         return False
